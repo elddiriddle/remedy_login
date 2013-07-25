b664a9d8f91c16ea4dcff70c7c531f93de6095e1
Bren Briggs <bbriggs@milestonepowered.com>
2013-07-25 19:48:43 -0400

    Added file Import-StoredCreds
    -Name may be changed later to match better with legal verbs + an actual
    description
    
    -File gathers a windows user's username, checks for a previously created
    credential file. If the file is not there, prompts user for password
    associated with that username. Hashes the password and stores it in a
    file for later use.
    
    -Calls file and stores password into memory for use in other scripts
    
    -To do: add description in comments for file

---
+++ Import-StoredCreds.ps1
@@ -0,0 +1,34 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+   [string]$UserName
+)
+If ($UserName -eq $null){
+	Do {
+		$UserName = Read-Host "Please Supply username for login"
+	}
+	Until ($UserName -ne $null)
+}
+# Enter location for all credential files. If statement verifies directory exists
+# and creates directory if it does not.
+$fileroot = "C:\scripts\credentials\"
+If ((Test-Path -path $fileroot) -ne $True){
+	New-Item $fileroot -type directory
+}
+
+# Checks to see if the User has the unique credential files and prompts to create them if they do not
+$user_cred_filepath = $fileroot + $UserName + "_credentials.txt" 
+if (!(Test-Path -Path $user_cred_filepath)){
+	Write-Host "Your credentials file for access with this username do not seem to exist..."
+	Write-Host ""
+	Write-Host " Please enter you credentials now; it is displayed and stored securely..."
+	read-host -assecurestring | convertfrom-securestring `
+| 	out-file -FilePath $user_cred_filepath
+}
+else {
+	Write-Host "Your credential file was found; proceeding..."
+}
+
+$READ_PASS_FILE = get-content $user_cred_filepath | convertto-securestring
+$CONVERT_PASSWORD = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($READ_PASS_FILE)
+$PASSWORD = [System.Runtime.InteropServices.Marshal]::ptrToStringAuto($CONVERT_PASSWORD)
